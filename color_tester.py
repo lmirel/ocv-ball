6d1a87462b577a500d797609085676246e33ca86
mirel <mirel.t.lazar@gmail.com>
2019-07-06 21:39:54 +0200

    improved tester

--- color_tester.py
+++ color_tester.py
@@ -4,26 +4,49 @@ from picamera import PiCamera
 import time
 import cv2
 import numpy as np
- 
- 
+import sys
+#from msvcrt import getch
+#import curses
+
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 32
 camera.rotation = 180
 rawCapture = PiRGBArray(camera, size=(640, 480))
- 
-while True:
-    while True:
+
+#stdscr = curses.initscr()
+#curses.cbreak()
+#stdscr.keypad(1)
+
+#stdscr.addstr(0,10,"Hit 'q' to quit")
+#stdscr.refresh()
+
+#key = ''
+
+if len(sys.argv) == 1:
+   hue_value = 1
+else:
+   for a in sys.argv[1:]:
+      hue_value = (int(a))
+estop = False
+
+while not estop:
+    while not estop:
         try:
-            hue_value = int(input("Hue value between 10 and 245: "))
-            if (hue_value < 1) or (hue_value > 254):
+            print ("hue value: ", hue_value)
+            #hue_value = int(input("Hue value between 0 and 255: "))
+            if (hue_value < 0) or (hue_value > 255):
                 raise ValueError
         except ValueError:
-            print("That isn't an integer between 10 and 245, try again")
+            estop = True
+            print("That isn't an integer between 0 and 255, try again")
+            exit ()
         else:
             break
- 
+
+#for hue_value in range (1, 255):
+#    print ("current hue: ", hue_value)
     lower_red = np.array([hue_value-10,100,100])
     upper_red = np.array([hue_value+10, 255, 255])
  
@@ -43,9 +66,19 @@ while True:
         cv2.imshow("Final Result", result)
  
         rawCapture.truncate(0)
- 
+
+        #key = stdscr.getch()
         k = cv2.waitKey(5) #& 0xFF
-        if "q" == chr(k & 255):
+        #print ("key: ", k)
+        if "q" == chr(k & 0xff):
+            estop = True
+            break
+        if "n" == chr(k & 0xff):
             break
       except KeyboardInterrupt:
+        estop = True
         break
+    time.sleep (1)
+    hue_value = hue_value + 1
+
+#curses.endwin()
